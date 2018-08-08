1fa15ca4dc38e56e68019fc6d06fbf00c702f39a
sjlutterbie <simon@ltrbe.com>
2018-08-08 23:16:09 +0000

    Footer static Component implemented. All static content implemented\!

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Header from './topnav/topNav_Components'
 import Container from './container/container_Components'
+import Footer from './footer/footer_Components'
 
 /**
  * App renders the top-level page structure
@@ -11,8 +12,10 @@ class App extends Component {
   render() {
     return (
     <div>
+    
       <Header.Header />
       <Container.Container />
+      <Footer.Footer />
 
     </div>
 
---
+++ client/src/footer/footer_Components.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    
+    render() {
+    
+        return (
+          
+            <footer>
+                <p>&copy; 2018 Simon Lutterbie</p>
+            </footer>
+            
+        );        
+    }
+}
+
+export default { Footer };
